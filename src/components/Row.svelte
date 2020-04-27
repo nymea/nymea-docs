5cb63dfa2a2ca434d3fbd2e6464c7df001638556
Lukas Mayerhofer <lukas.mayerhofer@nymea.io>
2020-04-27 08:23:54 +0200

    add grid components

---
+++ src/components/Row.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount, afterUpdate, setContext } from 'svelte';
+
+  let colsCount = 0;
+
+  afterUpdate(() => {
+    colsCount = document.querySelectorAll('.col').length;
+    setContext('colsCount', colsCount);
+    console.log('Row:afterUpdate()');
+  });
+</script>
+
+<style>
+  .row {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+</style>
+
+<div class="row">
+  <slot></slot>
+</div>
