0b06d987a098e18d9f3c37fd7a475e306fb2ecfa
Lukas Mayerhofer <lukas.mayerhofer@nymea.io>
2020-04-27 08:27:36 +0200

    change wording of navigation items and styling

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,13 +5,12 @@
 <style>
 	ul {
 		display: flex;
-		justify-content: space-between;
+		justify-content: flex-end;
 		list-style-type: none;
-		/* margin-left: -1.5rem; */
+		width: 100%;
 	}
 
 	li {
-		/* display: inline-block; */
 		margin-left: 1.5rem;
 	}
 
@@ -35,49 +34,6 @@
 		color: #202020;
 		text-decoration: underline;
 	}
-
-	/* nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	} */
 </style>
 
 <nav>
@@ -90,19 +46,19 @@
 		</li>
 		<li>
 			<a aria-current='{segment === "users" ? "page" : undefined}' href="users">
-				For Users
+				Users
 				<span>Smart Home</span>
 			</a>
 		</li>
 		<li>
 			<a aria-current='{segment === "developers" ? "page" : undefined}' href="developers">
-				For Developers
+				Developers
 				<span>Internet of Things</span>
 			</a>
 		</li>
 		<li>
 			<a aria-current='{segment === "businesses" ? "page" : undefined}' href="businesses">
-				For Businesses
+				Businesses
 				<span>Consumer Products</span>
 			</a>
 		</li>
