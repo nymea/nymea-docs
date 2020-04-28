2e17f4aa0766bcb11a95e62215bd70c6bdc628ae
Michael Zanetti <michael_zanetti@gmx.net>
2020-04-28 17:53:39 +0200

    add a little to the business section

--- src/routes/businesses/index.svelte
+++ src/routes/businesses/index.svelte
@@ -19,3 +19,72 @@
     </Row>
   </section>
 </Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2>Save thousands of integration hours</h2>
+        <p>nymea is ready to extend connectivity of your IoT device to virtually anything. Adding new integrations is easily done via over-the-air updates.</p>
+        <p>nymea is ready to be integrated on a variety of GNU/Linux based devices. Be it Yocto, Ubuntu Core, Debian or anything else, we've got the experience in delivering the nymea stack to your platform of choice.</p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2>The worlds smallest footprint</h2>
+        <p>A powerful, yet lean addon for your device firmware.</p>
+        <p>nymea does not require a power horse, it's perfectly fine with low cost modules as long as GNU/Linux is available for them.</p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2>Also need hardware?</h2>
+        <p>Together with our partners we have developed embedded devices ready to base your IoT product on.</p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2></h2>
+        <p></p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2></h2>
+        <p></p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
+<Wrapper>
+  <section>
+    <Row>
+      <Col offset="20" width="60">
+        <h2></h2>
+        <p></p>
+      </Col>
+    </Row>
+  </section>
+</Wrapper>
+
