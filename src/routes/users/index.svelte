0cae815ef2e4c3a4038a8e0e04b8866fbf97bf15
Michael Zanetti <michael_zanetti@gmx.net>
2020-05-11 13:57:51 +0200

    some work on the users page

--- src/routes/users/index.svelte
+++ src/routes/users/index.svelte
@@ -13,10 +13,12 @@
 <section>
   <Grid>
     <Row>
-      <Col>
-        <h1>Users</h1>
-        <p>Nymea gets you up and running with your Smart Home setup in no time.</p>
+      <Col middle>
+        <h3>Nymea gets you up and running with your Smart Home setup in no time.</h3>
         <a href="documentation/users/installation/gettingstarted">Learn more</a>
+      </Col>
+      <Col>
+        <img src="img/smart-home.png" alt="Smart home"/>
       </Col>
     </Row>
   </Grid>
@@ -26,10 +28,39 @@
   <Grid>
     <Row>
       <Col>
-        <h2>Mission</h2>
-        <p>Easy to use, yet powerful.<br>nymea does not require editing cryptic configuration files, it can be configured and controlled entirely using your smart phone or PC.</p>
-        <p>Local-first approach.<br>nymea ensures that your data is kept where it belongs, locally in your home. Besides the privacy advantage, this also keeps users independent form cloud vendors, and doesn't require an always-on internet connection.</p>
-        <p>Free and Open Source software.<br>nymea is comitted to open source software. Anything we do can be inspected, tuned and shared by users.</p>
+        <img src="img/plugnplay.svg" alt="Plug'n Play">
+      </Col>
+      <Col middle>
+        <h3>Easy to use, yet powerful</h3>
+        <p>Nymea does not require editing cryptic configuration files, it can be configured and controlled entirely using your smart phone or PC.</p>
+      </Col>
+    </Row>
+  </Grid>
+</section>
+        
+<section>
+  <Grid>
+    <Row>
+      <Col middle>
+        <h3>Local-first approach</h3>
+        <p>Nymea ensures that your data is kept where it belongs, locally in your home. Besides the privacy advantage, this also keeps users independent form cloud vendors, and doesn't require an always-on internet connection.</p>
+      </Col>
+      <Col>
+        <img src="img/shield.svg" alt="Privacy focused">
+      </Col>
+    </Row>
+  </Grid>
+</section>
+
+<section>
+  <Grid>
+    <Row>
+      <Col>
+        <img src="img/open-source.svg" alt="Open Source"/>
+      </Col>
+      <Col middle>
+        <h3>Free and Open Source</h3>
+        <p>Nymea is comitted to open source software. Anything we do can be inspected, tuned and shared by users.</p>
       </Col>
     </Row>
   </Grid>
@@ -40,10 +71,37 @@
     <Row>
       <Col>
         <h2>Features</h2>
-        <p>nymea is a connection talent. Connecting things to your smart home is easy and quick.</p>
-        <p>Define smart behaviors for your environment. Using easy to setup rules allows to create all sorts of scenarios. Turning on your light when it gets dark outside and you're at home is just one example. Just a few clicks and your custom behavior is ready to go. If that's not enough, nymea offers optional scripting support to create really advanced use cases. The possibilities are endless.</p>
+        <p>Nymea is a connection talent. Connecting things to your smart home is easy and quick.</p>
         <p>Advanced configuration. Even without the need of a smart home, nymea offers comprehensive configuration capabilities for your hardware. Headless network setup as client or access point, setting up an MQTT broker, easy remote access, running a web server, or updating your system is all right there at your fingertips when nymea is installed.</p>
         <p>Link to feature list</p>
+      </Col>
+    </Row>
+  </Grid>
+</section>
+
+<section>
+  <Grid>
+    <Row>
+      <Col>
+        <img src="img/magic.png" alt="It's magic"/>
+      </Col>
+      <Col middle>
+        <h3>It's magic!</h3>
+        <p>Define smart behaviors for your environment. Using easy to setup rules allows to create all sorts of scenarios. Turning on your light when it gets dark outside and you're at home is just one example. Just a few clicks and your custom behavior is ready to go.</p>
+      </Col>
+    </Row>
+  </Grid>
+</section>
+
+<section>
+  <Grid>
+    <Row>
+      <Col middle>
+        <h3>For the more advanced</h3>
+        <p>If simple smart rules aren't enough, nymea offers optional scripting support to create really advanced use cases. The possibilities are endless.</p>
+      </Col>
+      <Col>
+        <img src="img/scripting.png" alt="Scripting support"/>
       </Col>
     </Row>
   </Grid>
