968417f793f21183a5620f9b0a0f4c7d3b6d9a42
Lukas Mayerhofer <lukas.mayerhofer@nymea.io>
2020-06-04 09:48:13 +0200

    fix issue with title on integration pages

--- src/pages/documentation/_markdown.svelte
+++ src/pages/documentation/_markdown.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { route } from '@sveltech/routify';
   import TableOfContents from '../../_components/table-of-contents/TableOfContents.svelte';
   import { initCodeBlocks } from './_documentation-utils.js';
 
@@ -8,6 +9,8 @@
 <style>
 </style>
 
-<h1>{title}</h1>
-<TableOfContents />
+{#if $route.api.path.substring(0, $route.api.path.lastIndexOf('/')) !== '/documentation/overview/integrations'}
+  <h1>{title}</h1>
+  <TableOfContents />
+{/if}
 <slot />
