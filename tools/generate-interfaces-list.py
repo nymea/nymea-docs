7837ac6b2be9fc0cfaf81db0fd655c727b8ed325
Michael Zanetti <michael_zanetti@gmx.net>
2020-06-08 01:33:42 +0200

    Add a tree of interfaces

--- tools/generate-interfaces-list.py
+++ tools/generate-interfaces-list.py
@@ -44,6 +44,79 @@ def build_markdown(interfaces):
       ret += "See also: %s\n\n" % links
   return ret
 
+def insert_in_tree(tree, iface):
+  if "childs" not in iface:
+    iface["childs"] = []
+
+  if "extends" not in iface:
+    print("Adding to top level")
+    tree.append(iface)
+    tree.sort(key=lambda iface: iface["name"])
+    return True
+
+  if isinstance(iface["extends"], str):
+    for tmp in tree:
+      print("1 looking for iface", iface["extends"], "in", tmp["name"])
+      if tmp["name"] == iface["extends"]:
+        if iface not in tmp["childs"]:
+          tmp["childs"].append(iface)
+          tmp["childs"].sort(key=lambda iface: iface["name"])
+        return True
+      if insert_in_tree(tmp["childs"], iface):
+        return True
+    return False
+
+  found = 0
+  for extend in iface["extends"]:
+    for tmp in tree:
+      print("2 looking for iface", extend, "in", tmp["name"])
+      if tmp["name"] == extend:
+        if iface not in tmp["childs"]:
+          tmp["childs"].append(iface)
+          tmp["childs"].sort(key=lambda iface: iface["name"])
+        found = found + 1
+      if insert_in_tree(tmp["childs"], iface):
+        found = found + 1
+  if found is len(iface["extends"]):
+    return True
+  return False
+
+def build_tree(interfaces):
+  tree = []
+  doAgain = True
+  notInserted = {}
+  i = 0
+  while doAgain and i < 5:
+    i = i + 1
+    print("-------------")
+    doAgain = False
+    for ifaceName in interfaces:
+      print("inserting %s" % ifaceName)
+
+      iface = interfaces[ifaceName]
+      iface["name"] = ifaceName
+      inserted = insert_in_tree(tree, iface)
+      if not inserted:
+        print("NOT inserted")
+        notInserted[ifaceName] = iface
+      doAgain = doAgain or not inserted
+    interfaces = notInserted
+    print("tree is now %i of %i" % (len(tree), len(interfaces)))
+#    return tree
+  return tree
+
+
+def print_tree(tree, indent=0):
+  ret = ""
+  for iface in tree:
+    str = ""
+    for i in range(indent):
+      str += "  "
+    str += "* [" + iface["name"] + "](#" + iface["name"] + ")"
+    print(str)
+    ret += str + "\n\n"
+    ret += print_tree(iface["childs"], indent + 1)
+  return ret
 ### Main
 
 config = Utils.read_json("interfaces-config.json")
@@ -51,7 +124,10 @@ Utils.clone_repo(config["srcdir"])
 targets = Utils.find_targets(config["keyword"], config["outputdir"])
 print("Files to replace: %s" % targets)
 interfaces = load_interfaces(os.path.join(config["srcdir"], "nymea/libnymea/interfaces"))
-markdown = build_markdown(interfaces)
+tree = build_tree(interfaces)
+markdown = print_tree(tree)
+
+markdown += build_markdown(interfaces)
 replacements = {}
 replacements[config["keyword"]] = markdown
 Utils.generate_output_md(targets, replacements)
