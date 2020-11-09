0bfbfe138b6f9ce047cab59cdbc27fdb9598b7e5
Lukas Mayerhofer <lukas.mayerhofer@nymea.io>
2020-11-09 07:24:59 +0100

    move main.js to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import HMR from  '@sveltech/routify/hmr'
 import App from './App.svelte';
 
-const app = HMR(App, { target: document.body }, 'app')
+const app = HMR(App, { target: document.body }, 'app');
 
 export default app;
