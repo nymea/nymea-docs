d0d45b60286aec180af4097ebcec60a0311eaa46
Lukas Mayerhofer <lukas.mayerhofer@nymea.io>
2020-11-09 07:32:26 +0100

    update table of contents component

---
+++ src/_components/table-of-contents/index.ts
@@ -0,0 +1,3 @@
+export { default as TableOfContents } from './TableOfContents.svelte';
+export { default as TableOfContentsItems } from './TableOfContentsItems.svelte';
+export * from './_store.js';
